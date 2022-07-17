6c201a7b945b071daf72a3e10d0c884bfedc7dd1
khekhnev <m.a@khekhnev.ru>
2022-07-17 16:02:25 +0300

    Student info

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Maxim Khekhnev");
+        System.out.println("24.08.1996");
+    }
+}
