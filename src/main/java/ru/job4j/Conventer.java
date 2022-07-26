fba02686d4f395ef23f683d640590828c79d0449
khekhnev <m.a@khekhnev.ru>
2022-07-26 22:01:02 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/Conventer.java
+++ src/main/java/ru/job4j/Conventer.java
@@ -2,19 +2,22 @@ package ru.job4j;
 
 public class Conventer {
     public static float rubToEuro(float value) {
-        float res = value / 70;
-        return res;
-    }
+        return value / 70;
+           }
 
     public static float rubToDol(float value) {
-        float res = value / 60;
-        return res;
-    }
+        return value / 60;
+           }
 
     public static void main(String[] args) {
-        float euro = rubToEuro(140);
-        System.out.println("140 rubels are " + euro + " euro");
-        float dollars = rubToDol(140);
-        System.out.println("140 rubels are " + dollars + " dollars");
+        float in1 = 140;
+        float expected1 = 2;
+        float out = rubToEuro(in1);
+        boolean rsl1 = expected1 == out;
+        System.out.println("140 rubels are 2 euro " + rsl1);
+        float expected2 = 2.3333333f;
+        float out2 = rubToDol(in1);
+        boolean rsl2 = expected2 == out2;
+        System.out.println("140 rubels are 2.33 dollars " + rsl2);
     }
 }
