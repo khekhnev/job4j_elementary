1c2d0574e0a2acaddc82a4dddf6120649e83f711
khekhnev <m.a@khekhnev.ru>
2022-07-30 12:02:27 +0300

    1.2. Входные данные в тесте [#297750 #327103]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenA1B1C1X1() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -17,7 +17,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
 
-    public void whenA0B1C1X1() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -27,7 +27,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
 
-    public void whenA1B1C0X1() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -37,7 +37,7 @@ public class X2Test {
         Assert.assertEquals(expected, rsl);
     }
 
-    public void whenA1B1C1X0() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -45,5 +45,9 @@ public class X2Test {
         int expected = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void calc() {
     }
 }
