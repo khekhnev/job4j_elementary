86b3dcdd7fab96bb68a956bee2eac289799a983a
khekhnev <m.a@khekhnev.ru>
2022-07-30 07:40:35 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,16 +1,20 @@
 package ru.job4j;
 
+import static ru.job4j.math.MathFunction.*;
 public class Calculator {
+
+    public static double subAndDiv(double first, double second) {
+        return sum(sub(first, second), div(first, second));
+    }
+
+    public static double sumAll(double first, double second) {
+        return sum(first, second) + sub(first, second) + multiply(first, second) + div(first, second);
+    }
+
     public static void main(String[] args) {
-        int six = 6;
-        int five = 5;
-        int four = 4;
-        int two = 2;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        double ex = subAndDiv(314, 235);
+        System.out.println("Result of first example with 314 and 235 is " + ex);
+        ex = sumAll(314, 235);
+        System.out.println("result of second example with 314 and 235 is " + ex);
     }
 }
