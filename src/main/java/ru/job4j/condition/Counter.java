ab2019fc50793d714ed400ba6f8e922e8d00504e
khekhnev <m.a@khekhnev.ru>
2022-08-16 15:13:47 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/condition/Counter.java
+++ src/main/java/ru/job4j/condition/Counter.java
@@ -3,9 +3,20 @@ package ru.job4j.condition;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum;
-        int sum1 = start;
+        int sum1 = 0;
         for (sum = start; sum < finish; sum++) {
             sum1 = sum1 + (sum + 1);
+        }
+        return sum1;
+    }
+
+    public static int sumByEven(int start, int stop) {
+        int sum;
+        int sum1 = 0;
+            for (sum = start; sum <= stop; sum++) {
+            if (sum % 2 == 0) {
+                sum1 = sum1 + sum;
+            }
         }
         return sum1;
     }
@@ -14,5 +25,8 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
---
+++ src/test/java/ru/job4j/condition/CounterTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+
+    @Test
+    void whenSumEvenNumbersFrom1To10Then30() {
+        int out = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom7To13Then30() {
+        int out = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(out).isEqualTo(expected);
+    }
+}
