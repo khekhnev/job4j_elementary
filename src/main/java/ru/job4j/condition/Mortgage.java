fe50216f38731f0b9354a5acb3b41aed41a075bb
khekhnev <m.a@khekhnev.ru>
2022-08-19 08:55:25 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/condition/Mortgage.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Mortgage {
+        public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        while ((amount + amount * percent / 100)  > salary) {
+            amount = (amount + amount * percent / 100) - salary;
+            year++;
+        }
+            if (amount != 0) {
+                year++;
+            }
+        return year;
+    }
+
+    public static void main(String[] args) {
+
+            System.out.println(year(100, 120, 50));
+    }
+}
