edc97a6817bb32a5f2a8dbb2a48f8ca4adccc1dd
khekhnev <m.a@khekhnev.ru>
2022-08-20 10:28:10 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #333238]

--- src/main/java/ru/job4j/condition/Factorial.java
+++ src/main/java/ru/job4j/condition/Factorial.java
@@ -2,16 +2,11 @@ package ru.job4j.condition;
 
 public class Factorial {
     public  static int calc(int n) {
-        if (n != 0) {
-            int result = 1;
-            for (int i = 1; i <= n; i++) {
-                result = result * i;
-            }
-            return result;
-        } else {
-            int result = 1;
-            return result;
-            }
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
