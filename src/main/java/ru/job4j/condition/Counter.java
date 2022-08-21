e575497930561215fda8d82b809588a5ae555c73
khekhnev <m.a@khekhnev.ru>
2022-08-21 10:10:16 +0300

    5.0.1. Сумма чисел. [#226416 #332898]

--- src/main/java/ru/job4j/condition/Counter.java
+++ src/main/java/ru/job4j/condition/Counter.java
@@ -3,12 +3,11 @@ package ru.job4j.condition;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = start; i < finish; i++) {
+        for (int i = start; i <= finish; i++) {
             sum += i;
         }
         return sum;
     }
-
 
     public static int sumByEven(int start, int stop) {
         int sum = 0;
