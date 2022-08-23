dc60c5452295e347109c14e2150700be0fd26754
khekhnev <m.a@khekhnev.ru>
2022-08-23 15:20:37 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rsl;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rsl = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
