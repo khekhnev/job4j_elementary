d89d9eec85f4cc948d1df46c36d33b1662122bd4
khekhnev <m.a@khekhnev.ru>
2022-08-28 13:42:18 +0300

    6.7.4. Выигрышные комбинации в сокобан

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -32,4 +32,16 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        char[] win = extractDiagonal(board);
+        for (int i = 0; i < win.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
