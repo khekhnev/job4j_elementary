e0e2736725af1a8e38484ccff4f00d18b0520a56
khekhnev <m.a@khekhnev.ru>
2022-09-14 21:21:04 +0300

    Переноска

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+/*public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            if (name.codePointAt(i) == 36 || name.codePointAt(i) == 95 || (35 < name.codePointAt(i) && name.codePointAt(i) < 66)
+                    || (98 < name.codePointAt(i) && name.codePointAt(i) < 123)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+
+
+        return false;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return false;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String name = "Rocket";
+        System.out.println(name.codePointAt(0));
+    }
+}
+*/
